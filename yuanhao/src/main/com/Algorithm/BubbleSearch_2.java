2eb9aad4427478cc33e57dc6f4ec0ba60a8ecf49
Aicondo <202083020012@nuist.edu.cn>
2022-01-25 18:10:23 +0800

    add a new example of binary search

---
+++ yuanhao/src/main/com/Algorithm/BubbleSearch_2.java
@@ -0,0 +1,55 @@
+package main.com.Algorithm;
+import java.util.ArrayList;
+
+
+public class BubbleSearch_2 {
+
+        public static int find(ArrayList<String> array, String aim) {
+            int left = 0;
+            int right = array.size() - 1;
+
+            while(left <= right){
+                int middle = (left + right) / 2;
+                if (array.get(middle).equals(aim)){
+                    return middle;
+                }
+                else if (array.get(middle).compareTo(aim) < 0){
+                    left = middle + 1;
+                }
+                else if(array.get(middle).compareTo(aim) > 0){
+                    right = middle - 1;
+                }
+            }
+            return -1;
+        }
+
+        public static void main(String[] args) {
+            ArrayList<String> array = new ArrayList<>();
+            array.add("Allen");
+            array.add("Emma");
+            array.add("James");
+            array.add("Jeanne");
+            array.add("Kelly");
+            array.add("Kevin");
+            array.add("Mary");
+            array.add("Natasha");
+            array.add("Olivia");
+            array.add("Rose");
+
+            int result1 = find(array, "Kelly");
+            if (result1 == -1) {
+                System.out.println("Kelly 不存在名单中");
+            } else {
+                System.out.println("Kelly 存在名单中，位置是 " + result1);
+            }
+
+            int result2 = find(array, "Edith");
+            if (result2 == -1) {
+                System.out.println("Edith 不存在名单中");
+            } else {
+                System.out.println("Edith 存在名单中，位置是 " + result2);
+            }
+        }
+    }
+
+
